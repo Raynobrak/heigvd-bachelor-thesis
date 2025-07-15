f352cc82dd7d163b0f4d0a6a7d2a2c322384660e
Raynobrak <lcs.charbonnier@gmail.com>
2025-07-15 15:33:49 +0200

    fix main script

--- drone-rl-environment/src/drone_rl_environment/custom.py
+++ drone-rl-environment/src/drone_rl_environment/custom.py
@@ -15,25 +15,25 @@ from stable_baselines3 import PPO,DDPG
 
 from rl_utils import *
 
-print(os.getcwd())
+def run():
+    RUN_NAME = 'dqn-bignet-test'
+    STEP_PER_ITERATION = 16384 * 16
 
-RUN_NAME = 'dqn-night-run'
-STEP_PER_ITERATION = 16384
+    #model = load_model('ppo-tunnel-nightrun_ts-21037056_11-11-50-02.zip')
+    model = create_model()
 
-#model = load_model('ppo-tunnel-nightrun_ts-21037056_11-11-50-02.zip')
-model = create_model()
+    # simulation
+    i = 0
+    while True:
+        i += 1
+        # entraînement du modèle
+        train_model_in_environment(model, timesteps=STEP_PER_ITERATION, tb_run_name=RUN_NAME)
 
-# simulation
-i = 0
-while True:
-    i += 1
-    # entraînement du modèle
-    #train_model_in_environment(model, timesteps=STEP_PER_ITERATION, tb_run_name=RUN_NAME)
+        # sauvegarde
+        save_model(model, prefix=RUN_NAME+'_ts-'+str(i * STEP_PER_ITERATION))
 
-    # sauvegarde
-    #save_model(model, prefix=RUN_NAME+'_ts-'+str(i * STEP_PER_ITERATION))
+        # visualisation de la performance du drone dans une GUI
+        #visualize_model_in_environment(model, num_episodes=10)
 
-    # visualisation de la performance du drone dans une GUI
-    visualize_model_in_environment(model, num_episodes=10)
-
-
+if __name__ == '__main__':
+    run()
