838923f7d6ab9e85b5c5f9f91f9fde2eb5bfcf80
Raynobrak <lcs.charbonnier@gmail.com>
2025-07-21 09:35:04 +0200

    remove temp script to test map

--- drone-rl-environment/src/drone_rl_environment/map_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from mapping.map import Map
-
-map = Map(x_size=80, y_size=80, z_size=80, origin_offset=np.array([40,40,40]), resolution_voxels_per_unit=1) # todo : construire que si activé
-
-for i in range(10):
-    map.add_scan([[i,0,0]], [i,i,i], max_distance=100)
-
-map.save_2D_map_to_file()
